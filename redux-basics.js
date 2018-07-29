dbb6d47ac37de23dfb4094c49fb5767d0f6edf71
AndreyKonovalenko <andreinew@mail.ru>
2018-07-29 10:49:06 +0300

    242 setting up reducer and Store in example node running  file redux-basics.js

--- redux-basics.js
+++ redux-basics.js
@@ -1,6 +1,25 @@
-// Store
+const redux = require('redux');
+const createStore = redux.createStore;
+
+
+const initialState = {
+    counter: 0
+};
 
 // Reducer
+
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+};
+//"state = initialState" syntax for asigning default value,
+// if in any case variable is undefined it will take the default value
+
+// Store
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+
 
 //Dispatching Action
 
