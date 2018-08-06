4ae423e42f34d8cb3c8ac3761e7b417a32a37b34
Andrey <andreinew@mail.ru>
2018-08-06 15:03:06 +0000

    275 Handling Asynchronous Code by adding redus-thunk to the project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+// redux-thunk is a function for asynchronous action execution
+// by the way it is a middleware
+
 
 import './index.css';
 import App from './App';
@@ -28,7 +33,7 @@ const logger = store => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 // second argument in createStore is called "enhancer" where we can implement middleware or
 //list of middleware
 
