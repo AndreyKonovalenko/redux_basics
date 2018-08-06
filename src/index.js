205b0514ed001809f5a2e185dab2e9eb014fada9
Andrey <andreinew@mail.ru>
2018-08-06 11:03:58 +0000

    270 Adding middleware between store and aplication by using redux function applyMiddlewarew, for middleware we use sipmle logger function which logs changings in state

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -14,7 +14,21 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
-const store = createStore(rootReducer); 
+
+const logger = store => {
+    return next => {
+        return aciton => {
+            console.log('[Middleware] Dispatching', aciton);
+            const result = next(aciton);
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
+// second argument in createStore is called "enhancer" where we can implement middleware or
+//list of middleware
 
 ReactDOM.render(<Provider store={store}><App /></ Provider>, document.getElementById('root'));
 registerServiceWorker();
